beb68ea2c7d8a7d8601134f01169c104996a6a22
ahimmelstoss <ahimmelstoss@gmail.com>
2014-05-08 15:04:08 -0400

    lib got lost

---
+++ lib/global_substitution.rb
@@ -0,0 +1,7 @@
+percy_invitation = "The family of Percy Weasley proudly invite you to their graduation commencement on Saturday the 22nd of May 1993. Festivities will be held at The Burrow. See you then!"
+
+# Ron plans to have his party on May 18th, 1997 (Sunday). Write some chained .gsubs to make his invitation!
+
+ron_invitation = percy_invitation.gsub("Percy", "Ron").gsub("Saturday", "Sunday").gsub("22nd", "18th").gsub("1993", "1997")
+
+puts ron_invitation
