a6ef32c7ecf2655419b58b5db81c65eea0bc51e3
Katie Hoffman <ktahoffman@gmail.com>
2014-05-08 15:33:39 -0400

    deletes solutions

--- lib/string_interpolation.rb
+++ lib/string_interpolation.rb
@@ -3,11 +3,4 @@
 # Change the value of these variables to reflect Ron's info, he plans to have his party on May 18th, 1997 (Sunday).
 # Finally, print Ron's version of the invitation so he can copy it from his console.
 
-name = "Ron"
-day = "Sunday"
-date = "18th"
-year = "1997"
-
-invitation = "The family of #{name} Weasley proudly invite you to their graduation commencement on #{day} the #{date} of May #{year}. Festivities will be held at The Burrow. See you then!"
-
-puts invitation
+invitation = "The family of Percy Weasley proudly invite you to their graduation commencement on Saturday the 22nd of May 1993. Festivities will be held at The Burrow. See you then!"
