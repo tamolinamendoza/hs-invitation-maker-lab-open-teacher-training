6524c0770954c36c68067854a4d274e3961c7375
Katie Hoffman <ktahoffman@gmail.com>
2014-06-12 11:26:16 -0400

    remove error

--- lib/global_substitution.rb
+++ lib/global_substitution.rb
@@ -6,8 +6,7 @@ fred_invitation = percy_invitation.gsub("Percy", "Fred")
 # Ron plans to have his party on May 18th, 1997 (Sunday). 
 # Write some chained .gsubs to make his invitation!
 
-# To start, create a variable called ron_invitation and set it equal to 
-# the variable percy_invitation. Use the .gsub method to replace Percy's 
+# Use the .gsub method to replace Percy's 
 # info with Ron's info.
 
 ron_invitation = "replace this string with your code"
