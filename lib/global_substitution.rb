d8c41e4fccf55e81067aca4670d7f94f17e997a7
Katie Hoffman <ktahoffman@gmail.com>
2014-06-12 11:13:02 -0400

    improves instructions

--- lib/global_substitution.rb
+++ lib/global_substitution.rb
@@ -1,9 +1,17 @@
 percy_invitation = "The family of Percy Weasley proudly invite you to their graduation commencement on Saturday the 22nd of May 1993. Festivities will be held at The Burrow. See you then!"
 
-# Ron plans to have his party on May 18th, 1997 (Sunday). Write some chained .gsubs to make his invitation!
+# Fred used Percy's invitation too, but he only figured out how to replace Percy's name with his:
+fred_invitation = percy_invitation.gsub("Percy", "Fred")
 
-ron_invitation = # your code goes here
+# Ron plans to have his party on May 18th, 1997 (Sunday). 
+# Write some chained .gsubs to make his invitation!
 
-# Here we're calling puts on our variable so it will print in the console when we run our code!
+# To start, create a variable called ron_invitation and set it equal to 
+# the variable percy_invitation. Use the .gsub method to replace Percy's 
+# info with Ron's info.
 
+ron_invitation = "replace this string with your code"
+
+# Here we're calling puts on our variable so it will print in the console 
+# when we run our code!
 puts ron_invitation
--- lib/string_interpolation.rb
+++ lib/string_interpolation.rb
@@ -1,6 +1,27 @@
-# Instead of using .gsub, let's use string interpolation to change the content of the invitation. 
-# Set Percy's name, the day 'Saturday', the '22nd', and the year to be variables instead. 
-# Change the value of these variables to reflect Ron's info, he plans to have his party on May 18th, 1997 (Sunday).
-# Finally, print Ron's version of the invitation so he can copy it from his console.
+# It's 1998 and it's time for Ginny's graduation. Ron wants to help
+# his little sis out.
+
+# Instead of using .gsub, let's use string interpolation to change the content of the invitation.
+
+# If you need a refresher on interpolation, take a look at the `String Interpolation`
+# section in the readme.
+
+# Now that you know what string interpolation is, set the follow content into 
+# variables instead:
+# 1) name,
+# 2) the day 'Saturday'
+# 3) the '22nd'
+# 4) the year '1993' 
+
+# Great, it's time to change the value of these variables to reflect Ginny's info.
+# Ginny plans to have her party on May 17th, 1998 (Sunday).
+name = "replace with Ginny's name"
+day = "replace with Ginny's day"
+date = "replace with Ginny's date"
+year = "replace with Ginny's year"
 
 invitation = "The family of Percy Weasley proudly invite you to their graduation commencement on Saturday the 22nd of May 1993. Festivities will be held at The Burrow. See you then!"
+
+# Here we're calling puts on our variable, invitation, so it will print 
+# to the console when we run our code!
+puts invitation
