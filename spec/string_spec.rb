fc2b16a9b23aec927e78a0daeb4dceb2a7d99912
Katie Hoffman <ktahoffman@gmail.com>
2014-06-12 11:20:47 -0400

    update spec

--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe "global substitution" do 
   it "should print Ron's invitation" do 
-    STDOUT.should_receive(:puts).with("The family of Ron Weasley proudly invite you to their graduation commencement on Sunday the 18th of May 1997. Festivities will be held at The Burrow. See you then!")
+    expect($stdout).to receive(:puts).with("The family of Ron Weasley proudly invite you to their graduation commencement on Sunday the 18th of May 1997. Festivities will be held at The Burrow. See you then!")
     require_relative '../lib/global_substitution.rb'
   end
 end
 
 describe "string interpolation" do 
   it "should print Ron's invitation" do 
-    STDOUT.should_receive(:puts).with("The family of Ron Weasley proudly invite you to their graduation commencement on Sunday the 18th of May 1997. Festivities will be held at The Burrow. See you then!")
+    expect($stdout).to receive(:puts).with("The family of Ginny Weasley proudly invite you to their graduation commencement on Sunday the 17th of May 1998. Festivities will be held at The Burrow. See you then!")
     require_relative '../lib/string_interpolation.rb'
   end
 end
