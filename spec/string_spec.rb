6d4cf1d51c08c383cfd47f247375548d9c53569d
vicfriedman <victoria.m.friedman@gmail.com>
2014-07-25 10:04:32 -0400

    fixed test

--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -8,7 +8,7 @@ describe "global substitution" do
 end
 
 describe "string interpolation" do 
-  it "should print Ron's invitation" do 
+  it "should print Ginny's invitation" do 
     expect($stdout).to receive(:puts).with("The family of Ginny Weasley proudly invite you to their graduation commencement on Sunday the 17th of May 1998. Festivities will be held at The Burrow. See you then!")
     require_relative '../lib/string_interpolation.rb'
   end
