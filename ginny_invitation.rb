8bba83bc28e3c69e05e34a7c1aac05e35a1bfca4
vicfriedman <victoria.m.friedman@gmail.com>
2015-04-30 12:43:11 -0400

    solution

---
+++ ginny_invitation.rb
@@ -0,0 +1,6 @@
+invitation = "The family of #{name} Weasley proudly invite you to their graduation commencement on #{day_of_week} the #{date} of May #{year}. Festivities will be held at The Burrow. See you then!"
+
+name = 'Ginny'
+day_of_week = 'Sunday'
+date = '17th'
+year = '1998' 
---
+++ invitation.rb
@@ -0,0 +1,3 @@
+percy_invitation = "The family of Percy Weasley proudly invite you to their graduation commencement on Saturday the 22nd of May 1993. Festivities will be held at The Burrow. See you then!"
+
+percy_invitation.gsub("Saturday", "Sunday").gsub("22nd", "18th").gsub("1993", "1997").gsub("Percy", "Ron")
