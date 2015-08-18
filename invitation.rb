3d466e4e653627d6c421fa976a19ae805658679b
Victoria Friedman <victoria.m.friedman@gmail.com>
2015-08-18 11:57:21 -0400

    Revert "Create invitation.rb and ginny_invitation.rb and complete lab."

--- invitation.rb
+++
@@ -1,8 +0,0 @@
-#CHALLENGE 1
-percy_invitation = "The family of Percy Weasley proudly invite you to their graduation commencement on Saturday the 22nd of May 1993. Festivities will be held at The Burrow. See you then!"
-
-ron_invitation = percy_invitation.gsub("Percy Weasley", "Ron Weasley").gsub("22nd","18th").gsub("Saturday","Sunday").gsub("1993","1997")
-
-puts ron_invitation
-
-#CHALLENGE 2
